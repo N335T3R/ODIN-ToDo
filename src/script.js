db0305c22e13a49844bfaa5cd0d445e9d8378264
n335t3r <n335t3r@gmail.com>
2024-03-04 14:12:27 -0500

    html template/css updates; modal functionality implemented;

--- src/script.js
+++ src/script.js
@@ -6,15 +6,27 @@ import Item from "./classes/item";
 import storeData from "./utils/formDataUtils";
 import { extractData } from "./utils/formDataUtils";
 
+const addBtn = document.getElementById('addBtn');
+const exitBtn = document.getElementById('exitForm');
+const modal = document.getElementById('modal');
 const form = document.getElementById('form');
 const userItems = [];
 
-//Save Form Data
+// Open Form
+
+//Save Form Data as Item object
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     storeData();
     userItems.push(extractData());
     console.log(userItems);
 });
-
-
+// Open-Close Form Modal
+addBtn.addEventListener('click', () => {
+    modal.classList.add('modalOpen');
+    modal.classList.remove('modal');
+});
+exitBtn.addEventListener('click', () => {
+    modal.classList.add('modal');
+    modal.classList.remove('modalOpen');
+});
